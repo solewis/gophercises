4c3e0b960f2743184413be9e6c98282e34a94731
Steven Lewis <slewi1@monsanto.com>
2019-05-14 10:36:21 -0600

    Add shuffle capability to quiz app

--- quiz/main.go
+++ quiz/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"encoding/csv"
+	"math/rand"
 	"os"
 	"time"
 )
@@ -11,19 +12,21 @@ import (
 func main() {
 	csvPtr := flag.String("csv", "problems.csv", "path of csv file in the format of (question,answer). (default \"problems.csv\")")
 	limitPtr := flag.Int("limit", 30, "time limit in seconds (default 30)")
+	shufflePtr := flag.Bool("shuffle", false, "shuffles the questions each run")
 
 	flag.Parse()
 
-	csvFile, _ := os.Open(*csvPtr)
-	lines, _ := csv.NewReader(csvFile).ReadAll()
+	problems := parseProblems(*csvPtr, *shufflePtr)
+
+
 
 	timer := time.NewTimer(time.Duration(*limitPtr) * time.Second)
 
 	var correct int
 
 eventLoop:
-	for _, line := range lines {
-		fmt.Print("Problem #" + "1: " + line[0] + " = ")
+	for i, problem := range problems {
+		fmt.Printf("Problem #%d: %s = ", i + 1, problem.question)
 
 		answerCh := make(chan string)
 		go func() {
@@ -38,11 +41,37 @@ eventLoop:
 			fmt.Println("Times up!")
 			break eventLoop
 		case answer := <-answerCh:
-			if answer == line[1] {
+			if answer == problem.answer {
 				correct++
 			}
 		}
 	}
 
-	fmt.Printf("You scored %d out of %d\n", correct, len(lines))
+	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
+}
+
+func parseProblems(csvName string, shuffle bool) []problem {
+	csvFile, _ := os.Open(csvName)
+	lines, _ := csv.NewReader(csvFile).ReadAll()
+	problems := make([]problem, len(lines))
+	for i, line := range lines {
+		problems[i] = problem {
+			question:line[0],
+			answer:line[1],
+		}
+	}
+	if shuffle {
+		shuffledProblems := make([]problem, len(lines))
+		r := rand.New(rand.NewSource(time.Now().Unix()))
+		for i, r := range r.Perm(len(problems)) {
+			shuffledProblems[i] = problems[r]
+		}
+		return shuffledProblems
+	}
+	return problems
+}
+
+type problem struct {
+	question string
+	answer string
 }
