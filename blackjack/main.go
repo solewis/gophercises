263647932f4e32d5babfb7e282e2f9c1700cdaec
Steven Lewis <slewi1@monsanto.com>
2019-05-22 21:48:11 -0600

    Added ability to select number of rounds and which ai

--- blackjack/main.go
+++ blackjack/main.go
@@ -18,7 +18,6 @@ const (
 	maxBet                     money.USD = 50000
 	naturalBlackjackMultiplier float64   = 1.5
 	numDecks                   int       = 4
-	numRounds                  int       = 10000000
 	percentDeckUsage           float64   = .75
 )
 
@@ -36,16 +35,51 @@ func main() {
 	//fmt.Println("9. Early and late surrender allowed")
 	//fmt.Println("10. Continuous shuffle?")
 
+	rounds := determineRounds()
+	chosenAI := determineAI()
+
 	finalBalance := blackjack.Play(blackjack.Options{
 		MinBet:                     minBet,
 		MaxBet:                     maxBet,
 		NaturalBlackjackMultiplier: naturalBlackjackMultiplier,
 		NumDecks:                   numDecks,
-		NumRounds:                  numRounds,
+		NumRounds:                  rounds,
 		PercentDeckUsage:           percentDeckUsage,
-		AI:                         ai.Basic(),
+		AI:                         chosenAI,
 	})
 
 	fmt.Println("------FINAL BALANCE------")
 	fmt.Println(finalBalance.String())
+}
+
+func determineRounds() int {
+	fmt.Print("How many hands would you like to play? ")
+	var rounds int
+	for {
+		fmt.Scanln(&rounds)
+		switch {
+		case rounds < 0:
+			fmt.Println("Rounds must be more than 0")
+		default:
+			return rounds
+		}
+	}
+}
+
+func determineAI() blackjack.AI {
+	fmt.Println("Which AI would you like to use?")
+	ais := []blackjack.AI{ai.Human(), ai.Basic()}
+	for i, ai := range ais {
+		fmt.Printf("%d: %s\n", i+1, ai.Name())
+	}
+	var playerChoice int
+	for {
+		fmt.Scanln(&playerChoice)
+		switch {
+		case playerChoice > 0 && playerChoice <= len(ais):
+			return ais[playerChoice-1]
+		default:
+			fmt.Println("Invalid choice.")
+		}
+	}
 }
